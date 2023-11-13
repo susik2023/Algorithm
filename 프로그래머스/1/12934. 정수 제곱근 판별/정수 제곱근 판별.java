e0bd75952d0141b7f548049a7c87f8cbf98ac01b
susik2023 <146305978+susik2023@users.noreply.github.com>
2023-11-13 17:13:59 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double x = Math.sqrt(n);
+        if(x%1==0){
+            answer = (long)((x+1)*(x+1));
+        }else answer = -1;
+        return answer;
+    }
+}
