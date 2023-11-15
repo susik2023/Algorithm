44c87d14e898b43dba658e1bb77f19a61ec7b642
susik2023 <146305978+susik2023@users.noreply.github.com>
2023-11-15 23:17:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        int input = num;
+        if (input == 1) {
+            answer = 0;
+        } else if (input != 1) {
+            while (num != 1) {
+                if (num % 2 == 0) {
+                    num /= 2;
+                } else if (num % 2 == 1) {
+                    num = num * 3 + 1;
+                }
+                answer++;
+                if (answer == 500) {
+                    answer = -1;
+                    break;
+                }
+
+            }
+        }
+        return answer;
+    }
+}
