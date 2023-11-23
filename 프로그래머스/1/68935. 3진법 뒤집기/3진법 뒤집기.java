d14a187dccef668cea403b5bb664431201f79f49
susik2023 <146305978+susik2023@users.noreply.github.com>
2023-11-23 23:44:13 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.41 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String a = "";
+
+        while (n != 0) {
+            a += n % 3;
+            n /= 3;
+        }
+        answer = Integer.parseInt(a, 3);
+        return answer;
+    }
+}
