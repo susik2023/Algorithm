a3f13a9a452c57d67092e6896ab1c663791a757e
susik2023 <146305978+susik2023@users.noreply.github.com>
2023-12-27 22:47:18 +0900

    [level 1] Title: 2016년, Time: 1.54 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int a, int b) {
+
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30};
+        int totaldate = 0;
+        
+        for (int i = 0; i < a - 1; i++) {
+            totaldate += month[i];
+        }
+        
+        totaldate += b - 1;
+        
+        String answer = "";
+        
+        answer+=day[totaldate % 7];
+
+        
+        return answer;
+    }
+
+}
